5a6bcfb3d7de50b5148860eceee14e5869fd7890
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-10-19 11:27:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BackgroundScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BackgroundScreen(){
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "WEATHER APP",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.padding(13.dp))
+        BoxText()
+        Spacer(modifier = Modifier.padding(13.dp))
+        WeatherLocation()
+        Spacer(modifier = Modifier.padding(13.dp))
+        WeatherInfo()
+        Spacer(modifier = Modifier.padding(13.dp))
+    }
+}
+@Composable
+fun BoxText(){
+    Box(modifier = Modifier
+        .clip(RoundedCornerShape(10.dp))
+        .background(color = Color.Cyan)
+        .fillMaxWidth(),
+    ){
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+            val img = painterResource(R.drawable.img_background)
+            Image(painter = img, contentDescription = null,
+                modifier = Modifier.size(150.dp) )
+            Spacer(modifier = Modifier.padding(6.dp))
+            Text(text = "Rain", color = Color.Black, fontSize = 40.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(13.dp))
+        }
+    }
+}
+
+@Composable
+fun WeatherLocation(){
+    Text(text = "19 C", fontSize = 64.sp, fontWeight = FontWeight.Bold)
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = R.drawable.baseline_location_on_24), contentDescription = null)
+        Spacer(modifier = Modifier.padding(3.dp))
+        Text(text = "Yogyakarta", fontWeight = FontWeight.Bold, fontSize = 40.sp)
+
+    }
+    Spacer(modifier = Modifier.padding(13.dp))
+    Text(
+        text = "Kasihan, Kabupaten Bantul, Daerah Istimewa Yogyakarta",
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+        color = Color.Gray
+    )
+}
+@Composable
+fun WeatherInfo(){
+    Card(modifier = Modifier
+        .clip(RoundedCornerShape(10.dp))
+        .background(color = Color.Cyan)) {
+        Row() {
+            Column() {
+                Text(text = "Aw")
+            }
+        }
+    }
+}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherAppTheme {
+        BackgroundScreen()
+    }
+}
